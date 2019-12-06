dc2ec9dbf770d849764bf879d4c74c15f6d2b482
ifcassianasl <cassianasilveira64@gmail.com>
2019-12-06 17:15:55 -0300

    FrontEnd

--- myproject/web_list/urls.py
+++ myproject/web_list/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('new_search/', views.new_search, name='new_search'),
 ]
--- myproject/web_list/views.py
+++ myproject/web_list/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from .models import Search
+from bs4 import BeautifulSoup
 
 def home(request):
     return render(request, 'base.html')
+
+def new_search(request):
+    search = request.POST['search']
+    stuff_for_frontend = {
+        'search': search,
+    }
+    return render(request, 'web_list/new_search.html', stuff_for_frontend)
