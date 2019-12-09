9451cbb28c0f6c86c7f86a41a950c29d7a129335
ifcassianasl <cassianasilveira64@gmail.com>
2019-12-09 12:10:54 -0300

    Cool

--- myproject/web_list/views.py
+++ myproject/web_list/views.py
@@ -4,7 +4,8 @@ from bs4 import BeautifulSoup
 import requests
 from requests.compat import quote_plus
 
-BASE_CRAIGSLIST_URL = 'https://losangeles.craigslist.org/search/?query=python%20tutor'
+BASE_CRAIGSLIST_URL = 'https://losangeles.craigslist.org/search/?query={}'
+BASE_IMAGE_URL= 'https://images.craigslist.org/{}_300x300.jpg'
 
 def home(request):
     return render(request, 'base.html')
@@ -23,7 +24,26 @@ def new_search(request):
     post_titles = soup.find_all('a', {'class': 'result-title'})
     post_listings = soup.find_all('li', {'class': 'result-row'})
     
+    final_postings = []
+
+    for post in post_listings:
+        post_title = post.find(class_='result-title').text
+        post_url = post.find('a').get('href')
+        if post.find(class_='result-price'):
+            post_price = post.find(class_='result-price').text
+        else:
+            post_price = 'N/A'
+
+        if post.find(class_='result-image').get('data-ids'):
+            post_image_id = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
+            post_image_url = BASE_IMAGE_URL.format(post_image_id)
+        else:
+            post_image_url = 'http://craigslist.org/images/peace.jpg'
+
+        final_postings.append((post_title, post_url, post_price, post_image_url))
+    
     stuff_for_frontend = {
         'search': search,
+        'final_postings': final_postings
     }
     return render(request, 'web_list/new_search.html', stuff_for_frontend)
